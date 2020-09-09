eb5d51f5ceb73b801c450fd905dd6c4c78b84566
Lee Horn <lee.horn@softwire.com>
2020-09-09 19:23:01 +0100

    remove unnecessary route, set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
   resources 'books' do
     resources 'copies'
   end
+
+  root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
