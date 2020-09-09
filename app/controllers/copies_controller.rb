b28124fd8d96287fb87b2593c17275ca0fff21ce
Lee Horn <lee.horn@softwire.com>
2020-09-09 17:44:15 +0100

    copy editing

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -8,6 +8,19 @@ class CopiesController < ApplicationController
       render 'books/show'
     end
   end
+  def update
+    @copy = Copy.find(params[:id])
+    @copy.update(copy_params)
+    if @copy.save 
+      redirect_to @copy.book
+    else
+      render 'edit'
+    end
+  end
+  def edit 
+    @book = Book.find(params[:book_id])
+    @copy = Copy.find(params[:id])
+  end
   private
     def copy_params
       params.require(:copy).permit(:borrower,:due_date)
