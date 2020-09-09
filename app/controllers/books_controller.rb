253c3d49ea7a2e2035558d38d87dfcb35c82c036
Lee Horn <lee.horn@softwire.com>
2020-09-09 16:12:37 +0100

    adding books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
   def index
     @books = Book.all()
   end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params())
+    if @book.save()
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title,:author,:isbn)
+  end
 end
