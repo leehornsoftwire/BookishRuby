825e4870deec726226c57d515cdb9d0255e84e6f
Lee Horn <lee.horn@softwire.com>
2020-09-09 16:19:20 +0100

    editing books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,19 @@ class BooksController < ApplicationController
       render 'new'
     end
   end
+  def update 
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to edit_book_url
+    else
+      redirect_to @book
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def show
     @book = Book.find(params[:id])
   end
